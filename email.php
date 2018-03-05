5a5cd762ee32fc81959aeb5db6f707951cb73a26
fangquan <fangquan@server>
2018-03-05 14:05:10 -0500

    Add email.php, able to send emails from html. No form validation

---
+++ email.php
@@ -0,0 +1,46 @@
+<?php
+	$firstname = $_POST['firstname'];
+	$lastname = $_POST['lastname'];
+	$visitor_email = $_POST['email'];
+	$phone = $_POST['phone'];
+	$message = $_POST['message'];
+	
+	$email_from = $visitor_email;
+	$email_to = "fangquans@gmail.com, ethan.wang@nfeedu.com";
+	$email_subject = "New Form submission";
+
+	$email_body = "First name: $firstname.\n" . "Last name: $lastname\n"."Phone: $phone\n"."Email: $visitor_email\n"."$message";
+
+function IsInjected($str)
+{
+    $injections = array('(\n+)',
+           '(\r+)',
+           '(\t+)',
+           '(%0A+)',
+           '(%0D+)',
+           '(%08+)',
+           '(%09+)'
+           );
+               
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+    
+    if(preg_match($inject,$str))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+
+	mail($email_to,$email_subject,$email_body);
+	echo("Thank you! We will contact you soon!");
+?>
